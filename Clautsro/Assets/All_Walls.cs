8ceedc7f1dc4b39e441841e99e68f2e1b664138a
Ben Norris <mindputty@hotmail.com>
2015-02-01 20:36:20 -0500

    Added a script and a standard XML file
    All_Walls.cs will setup the scaling of the U-wall and the height of the
    ceiling. To set these, modify the scaleX and scaleZ (for scale of room
    size - width and length), in the XML file config.xml. To set ceiling
    scale, modify the scaleY in the XML file config.xml.

---
+++ Clautsro/Assets/All_Walls.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using System.Text;
+
+public class All_Walls : MonoBehaviour {
+
+	public TextAsset GameAsset;
+
+	float scaleX;
+	float scaleY;
+	float scaleZ;
+
+	void Start () {
+
+		ReadXML ("C:\\Users\\Owner\\Documents\\GitHub\\Rift\\Clautsro\\Assets\\config.xml");
+
+		Vector3 scaling = new Vector3();
+		scaling.x = scaleX;
+		scaling.y = scaleY;
+		scaling.z = scaleZ;
+
+		Debug.Log ("Scaling started! Scale X set to: " + scaleX);
+		Debug.Log ("Scale Y (ceiling) set to: " + scaleY);
+		Debug.Log ("Scale Z set to: " + scaleZ);
+		Transform trans = GetComponent <Transform> ();
+		trans.localScale.Scale(scaling);
+
+	}
+
+	void Update () {
+	
+	}
+
+	void ReadXML(string path){
+
+		var serializer = new XmlSerializer(typeof(XMLVector));
+		var stream = new FileStream(path, FileMode.Open);
+		var container = serializer.Deserialize(stream) as XMLVector;
+		stream.Close();
+
+		scaleX = container.scaleX;
+		scaleY = container.scaleY;
+		scaleZ = container.scaleZ;
+	}
+
+	[XmlRoot("scaleXYZ")]
+	public class XMLVector{
+		
+		public float scaleX;
+		public float scaleY;
+		public float scaleZ;
+	}
+}
+
+
